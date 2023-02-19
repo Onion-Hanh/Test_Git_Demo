755a13f66b87d78643f30eeeb14cf1c1a2ef34db
NET_NguyenDucHanh <RKDEV20221107@toanleh.onmicrosoft.com>
2023-02-19 18:00:46 +0700

    update index.js and index.html  for load list employee

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-alert("Xin chào các bạn nha!");
+var employees = [
+  { name: "Hirosima", age: 32 },
+  { name: "Bitzuma", age: 30 },
+  { name: "Kakaru", age: 29 },
+];
+var content_list_employee = "";
+for (var employee of employees) {
+  content_list_employee += "<li>" + employee.name + "</li>";
+}
+function loadEmployeeInfo() {
+  document.getElementById("list-employee").innerHTML = content_list_employee;
+}
+document.getElementById("btn_load").onclick = loadEmployeeInfo;
